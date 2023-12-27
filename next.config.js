276e71e219b5a97b2bc2c74567715888c3b26e91
sadegh akbari <akbarisadegh382@gmail.com>
2023-12-27 22:22:59 +0330

    fix: alias location bug

--- next.config.js
+++ next.config.js
@@ -1,5 +1,16 @@
 /** @type {import('next').NextConfig} */
+
+const path = require('path');
+
 const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, './'),
+    };
+
+    return config;
+  },
   images: {
     remotePatterns: [
       {
